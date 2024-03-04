f2e06ec41173707ee5c1787ff746ae6f1b089b22
danielschnurpfeil <dschnurp@email.cz>
2024-03-04 15:37:50 +0100

    app infrastructure (django backend, angular frontend) proposal

---
+++ backend/eu_inf_map/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EuInfMapConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'eu_inf_map'
---
+++ frontend/src/app/simple-view/simple-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-view',
+  templateUrl: './simple-view.component.html',
+  styleUrl: './simple-view.component.css'
+})
+export class SimpleViewComponent {
+
+}
