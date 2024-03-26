974d39a0e6a51dce053446b528089e763e1ae8d8
Daniel Schnurpfeil <dschnurp@email.cz>
2024-03-26 17:37:30 +0100

    simple bar chart

--- data_convert.py
+++ data_convert.py
@@ -19,7 +19,7 @@ def generate_json(dataframe):
     processes a DataFrame to generate a JSON data structure containing information about
     countries, years, total values, female values, and male values.
     """
-    json_data = []
+    json_data = {}
     for index, row in dataframe.iterrows():
         #  extracting the country name from the 'COUNTRY' column
         country = row['COUNTRY'][row['COUNTRY'].find(") ") + 2:]
@@ -29,7 +29,8 @@ def generate_json(dataframe):
         female = list(row[3::3].astype(str).replace(to_replace=["nan"], value="0").astype(float).to_dict().values())
         male = list(row[4::3].astype(str).replace(to_replace=["nan"], value="0").astype(float).to_dict().values())
 
-        json_data.append({"country": country, "years": years, "total": total, "female": female, "male": male})
+        json_data[country] = [{"year": i, "total": j, "male": k, "female": l} for i, j, k, l in
+                                    zip(years, total, male, female)]
 
     return json_data
 
@@ -39,8 +40,13 @@ universities_applied_sciences = generate_json(universities_applied_sciences)
 both_types = generate_json(both_types)
 
 data = {"retention": {"bachelor": {"RU": research_universities,
-                                    "UAS": universities_applied_sciences,
-                                    "RU + UAS": both_types}}}
+                                   "UAS": universities_applied_sciences,
+                                   "RU + UAS": both_types}}}
 
 with open('./data/data.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
+
+data = research_universities["Germany"]
+
+with open('./data/germany_research_unis.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=2)
---
+++ static/js/bar_chart.js
@@ -0,0 +1,137 @@
+var textvis = 
+
+{
+  "$schema": "https://vega.github.io/schema/vega/v5.json",
+  "width": 800,
+  "height": 200,
+  "padding": 5,
+
+  "data": [
+    {
+      "name": "germany_ru",
+      "values": [
+        {
+    "year": "2010/11",
+    "total": 0.9221348727,
+    "male": 0.9302007738,
+    "female": 0.8832001487
+  },
+  {
+    "year": "2011/12",
+    "total": 0.9056166507,
+    "male": 0.9081406854,
+    "female": 0.8940789474
+  },
+  {
+    "year": "2012/13",
+    "total": 0.8913994368,
+    "male": 0.8936696005,
+    "female": 0.8818724251
+  },
+  {
+    "year": "2013/14",
+    "total": 0.8681297879,
+    "male": 0.872053658,
+    "female": 0.853002451
+  },
+  {
+    "year": "2014/15",
+    "total": 0.871811232,
+    "male": 0.8758128517,
+    "female": 0.8573607933
+  },
+  {
+    "year": "2015/16",
+    "total": 0.8658528495,
+    "male": 0.8739535415,
+    "female": 0.838111546
+  },
+  {
+    "year": "2016/17",
+    "total": 0.8586060872,
+    "male": 0.8658218614,
+    "female": 0.8341646456
+  },
+  {
+    "year": "2017/18",
+    "total": 0.856122408,
+    "male": 0.8635984355,
+    "female": 0.8310124365
+  },
+  {
+    "year": "2018/19",
+    "total": 0.8512442775,
+    "male": 0.856281932,
+    "female": 0.8345899499
+  },
+  {
+    "year": "2019/20",
+    "total": 0.8688131088,
+    "male": 0.8778211525,
+    "female": 0.8391030014
+  },
+  {
+    "year": "2020/21",
+    "total": 0.865843097,
+    "male": 0.876456696,
+    "female": 0.8300391815
+  },
+
+      ]
+    }
+  ],
+
+
+  "scales": [
+    {
+      "name": "xscale",
+      "type": "band",
+      "domain": {"data": "germany_ru", "field": "year"},
+      "range": "width",
+      "padding": 0.05,
+      "round": true
+    },
+    {
+      "name": "yscale",
+      "domain": {"data": "germany_ru", "field": "total"},
+      "nice": true,
+      "range": "height"
+    }
+  ],
+
+  "axes": [
+    { "orient": "bottom", "scale": "xscale" },
+    { "orient": "left", "scale": "yscale" }
+  ],
+
+  "marks": [
+    {
+      "type": "rect",
+      "from": {"data":"germany_ru"},
+      "encode": {
+        "enter": {
+          "x": {"scale": "xscale", "field": "year"},
+          "width": {"scale": "xscale", "band": 1},
+          "y": {"scale": "yscale", "field": "total"},
+          "y2": {"scale": "yscale", "value": 0}
+        },
+        "update": {
+          "fill": {"value": "steelblue"}
+        },
+      }
+    },
+    {
+      "type": "text",
+      "encode": {
+        "enter": {
+          "align": {"value": "center"},
+          "baseline": {"value": "bottom"},
+          "fill": {"value": "#333"}
+        },
+
+      }
+    }
+  ]
+}
+
+  vegaEmbed('#textvis', textvis);
